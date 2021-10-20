beffdd8de0ee99844e02ddde4f62336d20bf8924
Bonganjalo Hadebe <bonganjaloh.hadebe@gmail.com>
2021-10-20 17:30:19 +0200

    feat(service): added express server

--- service.js
+++ service.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const AWS = require('aws-sdk');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/api/v1/', async (request, response) => {
+    response.status(200).json("Test server");
+});
+
+
+app.listen(4200, () => {
+    console.log(`API running on port 4200`);
+});
